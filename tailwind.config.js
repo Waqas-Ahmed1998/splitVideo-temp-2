bd53dfa691a6d9ea182f59005755df3e9d0ddc97
Waqas-Ahmed1998 <nizamaniwaqas4@gmail.com>
2022-04-06 23:04:00 +0500

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./*.html"],
   theme: {
+    extend: {
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
+      },
+      animation: {
+        wiggle: "wiggle 3s ease-in-out infinite",
+      },
+    },
     screens: {
       xs: "319px",
       iphone6: "370px",
