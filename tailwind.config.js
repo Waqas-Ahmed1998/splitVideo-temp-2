f699340ddefe62bc28004adbf07000afb25d172b
Waqas-Ahmed1998 <nizamaniwaqas4@gmail.com>
2022-04-12 02:21:00 +0500

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,14 @@ module.exports = {
           "0%, 100%": { transform: "rotate(-3deg)" },
           "50%": { transform: "rotate(3deg)" },
         },
+        slide: {
+          "0%": { transform: "translatex(0)", opacity: 0 },
+          " 100%": { transform: "translatex(7px)", opacity: 1 },
+        },
       },
       animation: {
         wiggle: "wiggle 3s ease-in-out infinite",
+        slide: "slide 1s ease-in-out infinite",
       },
     },
     screens: {
